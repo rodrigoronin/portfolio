90b968762671e99ff9e4e13da8f0e6b6921bdee2
rodrigoronin <liramailbox@gmail.com>
2026-01-22 17:46:15 -0300

    feat: base structure created
    added some styles without design system

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,21 @@
-import "./style.css";
+import TypeIt from "typeit";
+
+import "./variables.css";
+import "./index.css";
+
+new TypeIt(".animated", {
+  speed: 100,
+  startDelay: 500,
+  loop: true,
+})
+  .delete()
+  .type("Front-end", { delay: 1000 })
+  .delete()
+  .type("Back-end", { delay: 1000, startDelay: 500 })
+  .delete()
+  .type("Full Stack", { delay: 1000 })
+  .delete()
+  .type("Game", { delay: 1000 })
+  .delete()
+  .type("Mobile", { delay: 1000 })
+  .go();
