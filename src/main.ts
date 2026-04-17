941680ad19a65d97eafdb0fa2802fb679165dd9a
Rodrigo 'Kraby' Lira <liramailbox@gmail.com>
2026-04-17 12:01:16 -0300

    fix: fix animated typeit font
    added final texts and removed placeholders
    
    added github username to fetch public data

--- src/main.ts
+++ src/main.ts
@@ -8,14 +8,13 @@ new TypeIt(".animated", {
   startDelay: 500,
   loop: true,
 })
+  .type("stack", { delay: 1000 })
   .delete()
   .type("Front-end", { delay: 1000 })
   .delete()
-  .type("Back-end", { delay: 1000, startDelay: 500 })
+  .type("Back-end", { delay: 1000 })
   .delete()
-  .type("Fullstack", { delay: 1000 })
-  .delete()
-  .type("Game", { delay: 1000 })
+  .type("Game", { delay: 1000, startDelay: 500 })
   .delete()
   .type("Mobile", { delay: 1000 })
   .go();
